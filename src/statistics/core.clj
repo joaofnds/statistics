5e7b4f9c49052cf13bf3e57cadf68fb5e70a6eac
João Fernandes <joaofnds@joaofnds.com>
2021-04-05 19:57:38 -0300

    Fix core comment about quantiles

--- src/statistics/core.clj
+++ src/statistics/core.clj
@@ -11,4 +11,4 @@
 (comment
   (let [coll (sort [46 55 56 57 58 59 61 61 65 66 67 68 68 69 70 70 71 72 74 75 80])
         quantiles (i/prompt-coll "quantile: " i/prompt-float)]
-    (s/quantile-values coll quantiles)))
+    (zipmap quantiles (map #(s/icdf coll %) quantiles))))
