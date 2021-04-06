d45b1d12f64543b04997aa267d6be8f1268a7bf5
João Fernandes <joaofnds@joaofnds.com>
2021-04-05 21:40:37 -0300

    Update quantiles-table

--- src/statistics/vega.clj
+++ src/statistics/vega.clj
@@ -66,7 +66,7 @@
      [:div
       (fd-table fd)
       (mean-median-mode-table coll)
-      (quantiles-table coll [0.1 0.25 0.5 0.75 0.95])
+      (quantiles-table coll [0.25 0.5 0.75])
       (variance-table coll)
       [:vega-lite (build-chart fd)]]
      "out.html")))
