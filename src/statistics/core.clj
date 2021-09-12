17cb5f387e9252af68d593a95ab5c2038ff1baa3
João Fernandes <joaofnds@joaofnds.com>
2021-09-11 23:13:21 -0300

    exit after creating page

--- src/statistics/core.clj
+++ src/statistics/core.clj
@@ -1,12 +1,12 @@
 (ns statistics.core
-  (:require [clojure.pprint :as pprint]
-            [statistics.statistics :as s]
+  (:require [statistics.statistics :as s]
             [statistics.input :as i]
             [statistics.vega :as vega])
   (:gen-class))
 
 (defn -main [& args]
-  (vega/create-page (sort (map #(Float/parseFloat %) args))))
+  (vega/create-page (sort (map #(Float/parseFloat %) args)))
+  (System/exit 0))
 
 (comment
   (let [coll (sort [46 55 56 57 58 59 61 61 65 66 67 68 68 69 70 70 71 72 74 75 80])
